dbc6190681f6670bf44ddedab528aad588dabb17
Christian Reubrez <christian.reubrez@gmail.com>
2020-03-27 04:40:02 +0100

    new commit

---
+++ model/reportManager.php
@@ -0,0 +1,20 @@
+<?php
+require_once("model/Manager.php");
+class ReportManager extends Manager
+{
+    public function reporting($id)
+{  
+    $db = $this->dbConnect();   
+    $reporting=$db->prepare('UPDATE comments SET reporting=reporting+1 WHERE id = ?');
+    $reportCom=$reporting->execute(array($id));
+    return $reportCom;
+}
+
+public function deleteReporting($id)
+{
+    $db = $this->dbConnect();   
+    $reportingUpd=$db->prepare('UPDATE comments SET reporting=0 WHERE id = ?');
+    $repo=$reportingUpd->execute(array($id));
+    return $repo;
+}
+}
