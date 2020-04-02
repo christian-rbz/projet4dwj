2f6000b8d31ddfc0f9238a64aea5f95b71ff8527
Christian Reubrez <christian.reubrez@gmail.com>
2020-04-02 04:30:22 +0200

    page administrateur

---
+++ controller/controllerAdmin.php
@@ -0,0 +1,107 @@
+<?php
+
+require_once('model/postManager.php'); 
+require_once('model/commentManager.php');
+require_once('model/reportManager.php');
+require_once('model/connexionManager.php');
+
+
+
+function deleteUsers($id)
+{
+    $memberU=new MemberManager();
+    $del=$memberU->deleteUs($id); 
+    header('location:index.php?action=admin');    
+   
+}
+function deleteComments($idCom)
+{
+    $comment=new CommentManager();
+    $delComm= $comment->deleteCom($idCom);   
+    header('location:index.php?action=admin');   
+
+}
+function deletePosts($idPost)
+{   
+    $post=new PostManager();
+    $delPost=$post->deletePost($idPost);     
+   header('location:index.php?action=admin');
+}
+function editUser($idUs)
+{
+    $memberU=new MemberManager();
+    $sqedit=$memberU->editUs($idUs); 
+    require('view/backend/editUser.php');  
+    
+}
+function editPost($idPt)
+{ 
+    $postcom=new PostManager();
+    $sqeditpo=$postcom->editPosts($idPt);    
+    require('view/backend/editPost.php');    
+}
+function gotoAddPost()
+{
+    require('view/backend/addPost.php');
+}
+function addPost($title, $content)
+{ 
+    
+     
+        $title =   $_POST [ 'title' ]; 
+        $content  =  $_POST [ 'content' ];          
+        
+        if ( empty ( $title )   ||   empty ( $content ) )   { 
+                   
+            if ( empty ( $title ) )   { 
+                echo   "<font color='red'>Vous n'avez pas mis de titre.</font><br/>" ; 
+            } 
+           
+            if ( empty ( $content ) )   { 
+                echo   "<font color='red'>Vous n'avez pas mis votre chapitre.</font><br/>" ; 
+            } 
+           
+          
+        }   else   {  
+            $addPost=new PostManager();
+            $newPost=$addPost->addP($title, $content);                   
+         
+            echo   "<font color='green'>Le chapitre a été ajouté." ; 
+            echo   "<br/><a href='index.php'>Voir sur le site</a>" ; 
+           
+        } 
+       
+ 
+
+}
+function displayUpdate($idPt)
+{
+    $post=new PostManager();
+    $sqeditpo=$post->editPosts($idPt);  
+    require('view/backend/update.php'); 
+
+    if(!empty($_POST['title']) && !empty($_POST['content'])&&!empty($_POST['submit'])) {
+        $id=$_POST['id'];
+         $title=$_POST['title'];
+         $content=$_POST['content'];  
+         $post=new PostManager();
+         $postup=$post->upd($title,$content,$id);      
+         echo"La modification a été prise en compte !";      
+     
+     
+    } else {
+
+         echo "vous n'avez rien changer";
+         
+     }
+}
+function updRepo($id)
+{
+   $report= new ReportManager();
+   $repo= $report->deleteReporting($id);
+   header('Location: index.php?action=admin');
+}
+         
+            
+         
+    
